d25aaa224007747c7287b100f2bfe0f888d46815
Lina <akarabutova59@gmail.com>
2022-11-15 13:09:42 +0300

    implement smooth scrolling

--- js/index.js
+++ js/index.js
@@ -1,3 +1,45 @@
+// Mobile nav
+
+function showMenu(e) {
+  // const el = e.target;
+
+  // if (header.classList.contains("nav-open")) {
+  //   header.classList.remove("nav-open");
+  // } else {
+  //   header.classList.add("nav-open");
+  // }
+  //the same as above
+  header.classList.toggle("nav-open");
+}
+const header = document.querySelector(".header");
+const navBtn = document.querySelector(".btn-mobile-nav");
+navBtn.addEventListener("click", showMenu);
+
+//////////////////////////////////////////////////
+// Smooth scrolling
+const allLinks = document.querySelectorAll("a");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    // Scroll to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    //Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+    //Close mobile nav
+    if (link.classList.contains("main-nav-link"))
+      header.classList.toggle("nav-open");
+  });
+});
 // Fixing flexbox property missing in some Safari versions
 function checkFlexGap() {
   let flex = document.createElement("div");
@@ -15,17 +57,3 @@ checkFlexGap();
 
 const yearEl = document.querySelector(".year");
 yearEl.textContent = new Date().getFullYear();
-
-// Mobile nav
-function showMenu(e) {
-  // const el = e.target;
-  const header = document.querySelector(".header");
-  console.log(header.classList.contains("nav-open"));
-  if (header.classList.contains("nav-open")) {
-    header.classList.remove("nav-open");
-  } else {
-    header.classList.add("nav-open");
-  }
-}
-const navBtn = document.querySelector(".btn-mobile-nav");
-navBtn.addEventListener("click", showMenu);
